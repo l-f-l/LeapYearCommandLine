83cf312ee424656e0ea71c7ecdba5305fc83fba0
f <f@fs-MacBook-Pro.local>
2022-01-18 02:08:19 -0800

    first commit

---
+++ isleapyearc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int year = stoi(argv[1]);
+
+    if ((year % 4 == 0) && (year % 100 != 0))
+        cout << year << " was a leap year" << endl;
+    else if (year % 400 == 0)
+        cout << year << " was a leap year" << endl;
+    else
+        cout << year << " was not a leap year" << endl;
+
+    return 0;
+}
